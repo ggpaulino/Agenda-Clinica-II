31416e9a92daeddf4ba1ae5e497bd3245e506f3e
ggpaulino <ggpaulino@gmail.com>
2026-08-26 14:04:13 -0300

    Atualização no cadastro de fornecedores com inclusão de detectChanges() para garantir que a interface seja atualizada corretamente após a obtenção dos dados. Além disso, foi adicionada a função abrirFornecedores() no dashboard para navegação direta para a página de fornecedores.

--- FT/angular/src/app/pages/fornecedores/fornecedores.component.ts
+++ FT/angular/src/app/pages/fornecedores/fornecedores.component.ts
@@ -52,11 +52,13 @@ export class FornecedoresComponent implements OnInit {
       next: (data: any) => {
         this.fornecedores = data;
         this.carregando = false;
+        this.cdr.detectChanges();
       },
       error: (err) => {
         console.error('Erro ao carregar fornecedores:', err);
         this.erro = 'Não foi possível carregar os fornecedores.';
         this.carregando = false;
+        this.cdr.detectChanges();
       }
     });
   }
